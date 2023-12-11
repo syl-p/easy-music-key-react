899ae55ef4b9e0f23320e52535bbe3969eb57e63
syl-p <sylvain@atnos.com>
2023-12-11 12:57:43 +0100

    refactor: remove useless import

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,5 @@
-import { useState } from 'react'
-import Scale from "./utils/scale.class.js";
-import Degree from "./components/Degree.jsx";
 import Selector from "./components/Selector.jsx";
-import TonePlayer from "./utils/tone-player.class.js";
-import {createBrowserRouter, Outlet, Router, RouterProvider, useParams, useRouteError} from 'react-router-dom';
+import {createBrowserRouter, Outlet, RouterProvider, useParams, useRouteError} from 'react-router-dom';
 import MainContent from './components/MainContent.jsx';
 import DegreeList from './components/DegreeList.jsx';
 
